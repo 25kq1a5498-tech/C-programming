2d5fe1bff0bab5d8bb274948545a6c2d86696cb8
25kq1a5498-tech <25kq1a5498@pace.ac.in>
2025-11-24 14:41:21 +0530

    Add files via upload

---
+++ if else.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+     int a;
+     printf("enter a value");
+     scanf("%d",&a);
+     if(a>0)
+         printf("a is big number");
+     else
+         printf("a is small number");    
+    return 0;
+}

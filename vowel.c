42074ed7add96ef0489146c8e935ac17a1589cba
25kq1a5498-tech <25kq1a5498@pace.ac.in>
2025-11-27 17:20:33 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+ {
+    char ch;             
+    printf("enter char value:");  
+    scanf("%c",&ch);
+    switch(ch)
+    {
+        case 'a': 
+         printf("given is vowel");   
+         break;
+        case 'e':
+         printf("given is vowel");
+         break;
+        case 'i':
+         printf("given is vowel");
+         break;
+        case 'o':
+         printf("given is vowel");
+         break;
+        case 'u':
+         printf("given is vowel");
+         break;
+        default:
+         printf("given is consonant"); 
+ }
+      return 0;
+      }                                                                                   
+
+
+
+
+
+
+
+
+
+
+    
